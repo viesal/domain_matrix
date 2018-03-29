e21150147d231e2c93cd562710f1ccd126d1c14a
viesal <gviesal@gmail.com>
2018-03-29 17:05:38 +0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + '/static',
+    filename: "build.js"
+	},
+  watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [/node_modules/, /static/]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: [/static/]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: "url-loader"
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: "file-loader"
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+        ]
+    }
+};
